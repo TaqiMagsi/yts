85ac1f72f18062580ed9551cf390c1b0c97812ad
taqi <m.taqi@postex.pk>
2024-07-29 18:06:33 +0500

    search menu is hiding by using renderer2

--- src/app/module/header/header.component.ts
+++ src/app/module/header/header.component.ts
@@ -22,7 +22,12 @@ export class HeaderComponent {
 
   @HostListener('document:click', ['$event'])
   onClick(event: Event): void {
-   this.show=false
+  /*  this.show=false */
+  debugger
+  const myDiv = this.el.nativeElement.querySelector('#searchmenu');
+  if (myDiv) {
+    this.renderer.setStyle(myDiv,'display', 'none');
+  }
    this.search=''
    
   }
